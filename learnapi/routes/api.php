b3c96c6f392e53f12f8cba3f0fa7603083f7416e
ayush-vns <128778513+ayush-vns@users.noreply.github.com>
2024-02-15 18:33:04 +0530

    find and where qwery

--- learnapi/routes/api.php
+++ learnapi/routes/api.php
@@ -33,4 +33,47 @@ Route::get('/nbcc', function (Request $request){
 		return response()->json($error, 200);
 	}
 });
+Route::get('/find/{id}', function ($id,Request $request){
+	try
+	{
+		//$id=$request["id"];
+		$emp = ncc::find($id);
+		if($emp==null)
+		{
+			throw new Exception('Id Not Found');
+		}
+
+		$emp["status"]="ok";
+
+		return response()->json($emp, 200);
+	}
+	catch(\Exception $k) {
+		$error=array("status"=>"failed","error"=>$k->getMessage());
+		return response()->json($error, 200);
+	}
+});
+
+
+Route::get('/stm/{name}', function ($name,Request $request) {
+    try
+	{
+   
+//   $name=$request["name"];
+      $ser = ncc::where('name', $name)->first();
+  
+      if($ser==null)
+      {
+          throw new Exception('Id Not Found');
+      }
+
+      $ser["status"]="ok";
+
+      return response()->json($ser, 200);
+  }
+  catch(\Exception $k) {
+      $error=array("status"=>"failed","error"=>$k->getMessage());
+      return response()->json($error, 200);
+  }
+});
+  
 
